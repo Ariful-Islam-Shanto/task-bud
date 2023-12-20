3012e3d9cc7c4f27c0a761a6f48d324f2254e698
MD Ariful Islam Shanto <arifulislam1077@gmail.com>
2023-12-20 23:40:45 +0600

    Added a auth provider to maintain the auth related activity

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import routes from './Routes/routes.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./Routes/routes.jsx";
+import AuthProvider from "./AuthProvider/AuthProvider.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes}>
-    </RouterProvider>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={routes}></RouterProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
