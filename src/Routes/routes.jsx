e4ffb0ebd736319f76fd3adc2def5270cc6d330e
MD Ariful Islam Shanto <arifulislam1077@gmail.com>
2023-12-20 21:18:30 +0600

    Added a main layout and also created a routesfile for managing route

---
+++ src/Routes/routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from '../Layout/Layout';
+
+const routes = createBrowserRouter([
+    {
+        path : '/',
+        element : <Layout/>
+    }
+])
+
+export default routes;
