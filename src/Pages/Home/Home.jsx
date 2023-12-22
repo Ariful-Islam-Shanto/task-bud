c6d4b02e72d52db992b3f434ae3f6fbd4b370487
MD Ariful Islam Shanto <arifulislam1077@gmail.com>
2023-12-22 14:14:37 +0600

    Added a horizontal scroll bar using framer motion

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,10 +4,29 @@ import Navbar from "../../Components/Navbar/Navbar";
 import UserBenefits from "./UserBenefitsShowcase/UserBenefits";
 import Container from "../../Components/Container/Container";
 import Footer from "./Footer/Footer";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 const Home = () => {
+
+    const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress, {
+      stiffness: 100,
+      damping: 30,
+      restDelta: 0.001
+    });
+
   return (
     <div>
+              <motion.div style={{
+                 scaleX ,
+               position: "fixed",
+                 top: 0,
+                 left: 0,
+                 right: 0,
+                 height: "10px",
+                 background: "#3b82f6",
+                 transformOrigin : "0%",
+              }} className="progress-bar"  />
       <div
         style={{
           background: `url("https://i.postimg.cc/cJTZb0LS/Bannger.png")`,
